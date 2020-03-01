8fe467bf289626891fa01439f0390957a70d37e9
Aswin Gangadharan <keymonash9@gmail.com>
2020-03-01 18:57:04 +0530

    add refined images

--- pokedex/src/Pokecard.js
+++ pokedex/src/Pokecard.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import './Pokecard.css'
 
-const POKE_IMG = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+// const POKE_IMG = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+const POKE_IMG = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
+
+let padToThree = (num) => (num <= 999 ? `00${num}`.slice(-3): num);
 
 class Pokecard extends Component {
     render() {
-        let imgSrc = `${POKE_IMG}${this.props.id}.png`
+        let imgSrc = `${POKE_IMG}${padToThree(this.props.id)}.png`
         return (
             <div className="Pokecard">
                 <h1>{this.props.name}</h1>
